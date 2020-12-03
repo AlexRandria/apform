4b107e494c01f37ccd6be27e142e64d880ab9368
AlexRandria <randrianan.alex@gmail.com>
2020-12-03 16:47:12 +0100

    ajout nouvelle route

---
+++ src/Controller/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+class TestController
+{
+    public function index() 
+    {
+        dd('toto');
+    }
+
+    public function test()
+    {
+        dd('page de test');
+    }
+}
